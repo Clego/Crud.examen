764f7da90131f5d5e7a1ac27844d77f5db6e0664
Clego <dani.clem1212@gmail.com>
2025-02-17 18:56:14 +0100

    Añadida funcionalidad a input

--- src/input/Input.java
+++ src/input/Input.java
@@ -1,6 +1,19 @@
 package input;
 
+import java.util.Scanner;
+
 public class Input {
 
+    Scanner sc = new Scanner(System.in);
+
+public int numberInput () {
+    int num = sc.nextInt();
+    return num;
+}
+
+public String stringInput () {
+    String str = sc.next();
+    return str;
+}
 
 }
